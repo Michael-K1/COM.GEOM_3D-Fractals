f16f734e8a19d8c10e65123134cf2572c1b4f602
Michael Daniel Naguib <michael.danielnaguib@gmail.com>
2020-08-13 19:52:22 +0200

    ADD: HUD for Tetrahedron
    ADD: prefabs for color and position sliders

---
+++ Assets/Scripts/HUD/FractalHUD.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FractalHUD : MonoBehaviour{
+    [SerializeField]
+    private FractalVisualizer fv;
+
+    protected FractalVisualizer Fv{
+        get{
+            if (!fv)
+                fv=GetComponentInParent<GlobalHUD>().fv;
+            return fv;
+        }
+    }
+
+    public Toggle showFractal;
+    public Slider posX, posY, posZ;
+    public ColorHUD fractalColor;
+    
+}
---
+++ Assets/Scripts/HUD/TetraHUD.cs
@@ -0,0 +1,30 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class TetraHUD : FractalHUD{
+    [Header("Tetrahedron")]
+    public Slider tetraIterations;
+    public Slider tetraScale;
+    private void Start(){
+        
+        showFractal.isOn = Fv.showTetra;
+        posX.value = Fv.tetrahedronPos.x;
+        posY.value = Fv.tetrahedronPos.y;
+        posZ.value = Fv.tetrahedronPos.z;
+        tetraIterations.value = Fv.tetraIterations;
+        tetraScale.value = Fv.tetraScale;
+        fractalColor.SetUp(Fv.tetraColor);
+    }
+
+    private void LateUpdate(){
+        Fv.showTetra = showFractal.isOn;
+        Fv.tetrahedronPos.x = posX.value;
+        Fv.tetrahedronPos.y = posY.value;
+        Fv.tetrahedronPos.z = posZ.value;
+        Fv.tetraIterations =(int) tetraIterations.value;
+        Fv.tetraScale = tetraScale.value;
+
+    }
+}
