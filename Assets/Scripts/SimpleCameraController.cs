55e1fb3c65d224c060850f171e004ab0a8d17305
Michael Daniel Naguib <michael.danielnaguib@gmail.com>
2020-09-30 15:29:50 +0200

    ADD: audio
    UPDATE: removed scroll wheel option in Camera

--- Assets/Scripts/SimpleCameraController.cs
+++ Assets/Scripts/SimpleCameraController.cs
@@ -120,13 +120,13 @@ namespace UnityTemplateProjects
             }
 
             // Hide and lock cursor when right mouse button pressed
-            if (Input.GetMouseButtonDown(1))
+            if (Input.GetMouseButtonDown(1) || Input.GetKeyDown(KeyCode.LeftControl))
             {
                 Cursor.lockState = CursorLockMode.Locked;
             }
 
             // Unlock and show cursor when right mouse button released
-            if (Input.GetMouseButtonUp(1))
+            if (Input.GetMouseButtonUp(1) || Input.GetKeyUp(KeyCode.LeftControl))
             {
                 Cursor.visible = true;
                 Cursor.lockState = CursorLockMode.None;
@@ -153,7 +153,7 @@ namespace UnityTemplateProjects
             }
             
             // Modify movement by a boost factor (defined in Inspector and modified in play mode through the mouse scroll wheel)
-            boost += Input.mouseScrollDelta.y * 0.2f;
+            //boost += Input.mouseScrollDelta.y * 0.2f;
             translation *= Mathf.Pow(2.0f, boost);
 
             m_TargetCameraState.Translate(translation);
